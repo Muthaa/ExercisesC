cecb1b8f7696b94b279cfa165ac7b6c6294c7c5d
Philip Kyalo <32406905+Muthaa@users.noreply.github.com>
2024-04-29 11:51:35 +0300

    Create FizzBuzz.cs

---
+++ FizzBuzz.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FizzBuzz(50); // Example usage with upto = 16
+    }
+
+    static void FizzBuzz(int upto)
+    {
+        for (int i = 1; i < upto; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
